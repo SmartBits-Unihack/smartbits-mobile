7a50803cf71d09a59a214fdaa2532faae1f36d9e
richardbaczur1 <richardbaczur1@gmail.com>
2022-11-26 18:38:31 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# smartbits-mobile" 
---
+++ app/src/main/java/mbr/personal/unihack_smartbits/MainActivity.java
@@ -0,0 +1,14 @@
+package mbr.personal.unihack_smartbits;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
